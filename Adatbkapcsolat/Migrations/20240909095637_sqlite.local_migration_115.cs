043b514dac6db8cf4fe430c2f098133c86a39562
Marcellnagy05 <marcika863@gmail.com>
2024-09-09 11:57:35 +0200

    sql connection done

---
+++ Adatbkapcsolat/Migrations/20240909095637_sqlite.local_migration_115.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adatbkapcsolat.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlitelocal_migration_115 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Adatbkapcsolat/migration_script.sql
+++ Adatbkapcsolat/migration_script.sql
@@ -3,3 +3,24 @@
     "ProductVersion" TEXT NOT NULL
 );
 
+BEGIN TRANSACTION;
+
+CREATE TABLE "Users" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Users" PRIMARY KEY AUTOINCREMENT,
+    "Name" TEXT NOT NULL,
+    "BirthDate" TEXT NOT NULL,
+    "Email" TEXT NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240909095108_sqlite.local_migration_757', '8.0.8');
+
+COMMIT;
+
+BEGIN TRANSACTION;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240909095637_sqlite.local_migration_115', '8.0.8');
+
+COMMIT;
+
